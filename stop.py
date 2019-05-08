2e04fab2d6c7fe8ea92ee82ace541b1f19f262f8
Bhagya <bhagyanadukandy@gmail.com>
2019-05-08 17:24:54 +0530

    stop word finished

--- stop.py
+++ stop.py
@@ -1,8 +1,18 @@
 import spacy
-spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+nlp = spacy.load("en_core_web_sm")
 
-#Printing the total number of stop words:
-print('Number of stop words: %d' % len(spacy_stopwords))
 
-#Printing first ten stop words:
-print('First ten stop words: %s' % list(spacy_stopwords)[:20])
+
+text = """When learning data science, you shouldn't get discouraged!
+Challenges and setbacks aren't failures, they're just part of the journey. You've got this!"""
+
+
+doc = nlp(text)
+
+
+filtered_sent=[]
+
+for word in doc:
+    if word.is_stop==False:
+        filtered_sent.append(word)
+print("Filtered Sentence:",filtered_sent)
